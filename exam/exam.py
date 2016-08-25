06ab6f1e95edaaaa07cfe19ed2bc95d78d86a138
sunil <n16106@std.it-college.ac.jp>
2016-08-25 12:07:23 +0900

    hello

--- exam/exam.py
+++ exam/exam.py
@@ -1,23 +1,27 @@
-# 'hello, {name}!'と出力してください 。
+def hello (name):
+    print("hello, {01|".formate(name))
 def hello(name):
-    pass
+    print(len('sentence'))
 
-
-# sentence の文字数を出力してください
+def length(sentence):
+    print('sentence'[2:5])
 def length(sentence):
     pass
 
 
-# sentence の2文字目から5文字目まで(5文字目は含まない)を出力してください
+def slicing2to5(sentence):
+    pass
 def slicing2to5(sentence):
     pass
 
 
-# number の符号を出力してください。ただし、0は'0'と出力してください
+def list_sort(data)
+       return data.sort()
+
 def number_sign(number):
     pass
 
 
-# number が素数なら'ok',そうでないなら'ng'と出力してください
+def prime_number(number):
 def prime_number(number):
     pass
